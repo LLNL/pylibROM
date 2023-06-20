8f1847760b62f7549347fe6ee5e37f65fc76b67f
pravija12345 <pravija123@tamu.edu>
2023-06-20 11:16:40 -0500

    added bindings for BasisGenerator and Options class

---
+++ bindings/pylibROM/linalg/pyOptions.cpp
@@ -0,0 +1,43 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <pybind11/operators.h>
+#include <pybind11/stl.h>
+#include "linalg/Options.h"
+
+namespace py = pybind11;
+using namespace CAROM;
+       
+void init_Options(pybind11::module_ &m) {
+
+    py::class_<Options>(m, "Options")
+        .def(py::init<int, int, int, bool, bool>(), py::arg("dim_"), py::arg("samples_per_time_interval_"),py::arg("max_time_intervals_") = -1,py::arg("update_right_SV_") = false, py::arg("write_snapshots_") = false)
+        .def_readwrite("dim", &Options::dim)
+        .def_readwrite("samples_per_time_interval", &Options::samples_per_time_interval)
+        .def_readwrite("max_time_intervals", &Options::max_time_intervals)
+        .def_readwrite("update_right_SV", &Options::update_right_SV)
+        .def_readwrite("write_snapshots", &Options::write_snapshots)
+        .def_readwrite("max_basis_dimension", &Options::max_basis_dimension)
+        .def_readwrite("singular_value_tol", &Options::singular_value_tol)
+        .def_readwrite("debug_algorithm", &Options::debug_algorithm)
+        .def_readwrite("randomized", &Options::randomized)
+        .def_readwrite("randomized_subspace_dim", &Options::randomized_subspace_dim)
+        .def_readwrite("random_seed", &Options::random_seed)
+        .def_readwrite("linearity_tol", &Options::linearity_tol)
+        .def_readwrite("initial_dt", &Options::initial_dt)
+        .def_readwrite("sampling_tol", &Options::sampling_tol)
+        .def_readwrite("max_time_between_samples", &Options::max_time_between_samples)
+        .def_readwrite("fast_update", &Options::fast_update)
+        .def_readwrite("skip_linearly_dependent", &Options::skip_linearly_dependent)
+        .def_readwrite("save_state", &Options::save_state)
+        .def_readwrite("restore_state", &Options::restore_state)
+        .def_readwrite("min_sampling_time_step_scale", &Options::min_sampling_time_step_scale)
+        .def_readwrite("sampling_time_step_scale", &Options::sampling_time_step_scale)
+        .def_readwrite("max_sampling_time_step_scale", &Options::max_sampling_time_step_scale)
+        .def("setMaxBasisDimension", &Options::setMaxBasisDimension, py::arg("max_basis_dimension_"))
+        .def("setSingularValueTol", &Options::setSingularValueTol, py::arg("singular_value_tol_"))
+        .def("setDebugMode", &Options::setDebugMode,  py::arg("debug_algorithm_"))
+        .def("setRandomizedSVD", &Options::setRandomizedSVD, py::arg("randomized_"), py::arg("randomized_subspace_dim_") = -1, py::arg("random_seed_") = 1)
+        .def("setIncrementalSVD", &Options::setIncrementalSVD, py::arg("linearity_tol_"), py::arg("initial_dt_"), py::arg("sampling_tol_"), py::arg("max_time_between_samples_"), py::arg("fast_update_") = false, py::arg("skip_linearly_dependent_") = false)
+        .def("setStateIO", &Options::setStateIO,py::arg("save_state_"),py::arg("restore_state_"))
+        .def("setSamplingTimeStepScale", &Options::setSamplingTimeStepScale,py::arg("min_sampling_time_step_scale_"),py::arg("sampling_time_step_scale_"),py::arg("max_sampling_time_step_scale_"));
+}
---
+++ tests/test_BasisGenerator.py
@@ -0,0 +1,78 @@
+import sys
+sys.path.append("..")
+
+import build.pylibROM.linalg as libROM
+import numpy as np 
+
+# Create an instance of BasisGenerator
+options = libROM.Options(4, 20, 3, True, True)
+incremental =   False
+basis_file_name = "basis.h5"
+file_format = libROM.Formats.HDF5
+generator = libROM.BasisGenerator(options, incremental, basis_file_name,file_format)
+
+
+# Test the isNextSample method
+time = 1.0  # Time of interest
+is_next = generator.isNextSample(time)
+print("isNextSample:", is_next)
+
+# Test the updateRightSV method
+update_success = generator.updateRightSV()
+print("updateRightSV success:", update_success)
+
+# Test the takeSample method
+time = 1.0  # Time of the state
+dt = 0.1  # Time step
+u_in_data = np.array([1.0, 2.0, 3.0])
+result = generator.takeSample(u_in_data, time, dt)
+print("takeSample result:", result)
+
+# Call the writeSnapshot function
+generator.writeSnapshot()
+
+# Call the loadSamples function
+# base_file_name = "basis.h5_snapshot"
+# kind = "snapshot"
+# cut_off = 10
+# db_format = libROM.Formats.HDF5
+# generator.loadSamples(base_file_name, kind, cut_off, db_format)
+
+# Call the computeNextSampleTime function
+u_in = [1.0, 2.0, 3.0]
+rhs_in = [0.1, 0.2, 0.3]
+time = 0.0
+next_sample_time = generator.computeNextSampleTime(u_in, rhs_in, time)
+print("computeNextSampleTime",next_sample_time)
+
+# Get the spatial basis
+spatial_basis = generator.getSpatialBasis()
+print("spatial_basis",spatial_basis.get_data())
+
+# Get the temporal basis
+temporal_basis = generator.getTemporalBasis()
+print("temporal_basis",temporal_basis.get_data())
+
+# Get the singular values
+singular_values = generator.getSingularValues()
+print("singular_values",singular_values.get_data())
+
+# Get the snapshot matrix
+snapshot_matrix = generator.getSnapshotMatrix()
+print("snapshot_matrix",snapshot_matrix.get_data())
+
+# Get the number of basis time intervals
+num_intervals = generator.getNumBasisTimeIntervals()
+print("Number of basis time intervals:", num_intervals)
+
+# Get the basis interval start time for a specific interval
+interval = 0  # Replace with the desired interval index
+interval_start_time = generator.getBasisIntervalStartTime(interval)
+print("Start time for interval", interval, ":", interval_start_time)
+
+# Get the number of samples taken
+num_samples = generator.getNumSamples()
+print("Number of samples:", num_samples)
+
+# Test the endSamples method
+generator.endSamples()
