a0e37a12b8e6cdcca8e4c3bdf29f2486be1b5117
Kevin Chung <chung28@llnl.gov>
2023-08-16 13:27:59 -0700

    PointwiseSnapshot python class.

--- bindings/pylibROM/mfem/__init__.py
+++ bindings/pylibROM/mfem/__init__.py
@@ -2,3 +2,4 @@
 # either define/import the python routine in this file.
 # This will combine both c++ bindings/pure python routines into this module.
 from .Utilities import ComputeCtAB
+from .PointwiseSnapshot import PointwiseSnapshot
