1c8583e32c4ee0c4b8c8e5b42ced6655795efa31
Siu Wun Cheung <cheung26@llnl.gov>
2023-09-11 11:01:03 -0700

    Update examples

--- examples/dmd/nonlinear_elasticity.py
+++ examples/dmd/nonlinear_elasticity.py
@@ -10,12 +10,19 @@
    How to run:
       mpirun -np 8 python <arguments>
 
-   Example of arguments:
+      Example 1 of arguments:
       nonlinear_elasticity.py -s 2 -rs 1 -dt 0.01 -tf 5 -visit
    
       Output 1:
-      Relative error of DMD position (x) at t_final: 5 is 6.95681e-05
-      Relative error of DMD velocity (v) at t_final: 5 is 0.00139776
+      Relative error of DMD velocity (v) at t_final: 5.000000 is 1.145E-03
+      Relative error of DMD position (x) at t_final: 5.000000 is 7.371E-05
+
+      Example 2 of arguments:
+      nonlinear_elasticity.py -s 2 -rs 1 -dt 0.01 -tf 5 -nwinsamp 10 -visit
+   
+      Output 2:
+      Relative error of DMD velocity (v) at t_final: 5.000000 is 1.106E-04
+      Relative error of DMD position (x) at t_final: 5.000000 is 4.304E-06
 '''
 import sys
 import mfem.par as mfem
